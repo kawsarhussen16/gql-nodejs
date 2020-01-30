3b3b8671859bafc0d57991793e8d1a85ed2425ba
Kawsar Hussen <kawsarhussen@gmail.com>
2020-01-30 00:14:03 -0500

    Data Schema created, schema connected with graphql, exiting app is also passed through graphql middleware

--- server.js
+++ server.js
@@ -2,15 +2,43 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config({ path : 'variable.env'})
-const bodyparser = require('body=parser');
+const bodyParser = require('body-parser');
+const { ApolloServer, makeExecutableSchema } = require('apollo-server-express');
+
+const Recipe = require("./models/recipe.js");
+const User = require("./models/user.js");
+const {typeDefs} = require("./schema");
+const {resolvers}  = require('./resolvers'); 
+
+
+//Create Schema
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+//connect schemas with graphql
+const server = new ApolloServer({
+        schema,
+        context: {
+            Recipe, User
+        }
+})
+const path = '/graphql';
+
+app.use(bodyParser.json())
+server.applyMiddleware({
+    app, path, // app is from an existing express app
+  });
+  
 mongoose
     .connect(process.env.MONGO_URI)
     .then( ()=> console.log('Db connected'))
     .catch( err => console.log(err))
 
+
+
 const PORT = process.env.PORT || 4000;
-
-
 
 app.listen(PORT, ()=> {
     console.log(`Server is listening on Port ${PORT}`);
