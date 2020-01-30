e65f8c87ac9cb6ad354e2fc0a0da8a4a3606c154
Kawsar Hussen <kawsarhussen@gmail.com>
2020-01-30 02:38:21 -0500

    React App created and Appollo client ecosystem implemented

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider}  from'react-apollo'
+
+const client=  new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+})
+
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+     
+    
+    document.getElementById('root'));
+
---
+++ client/src/queries/index.js
@@ -0,0 +1,18 @@
+import {gql} from 'apollo-boost';
+
+
+
+export const GET_ALL_RECIPES = gql`
+    query{
+        getAllRecipes {
+            name
+            description
+            instructions
+            category
+            likes
+            createdDate
+        }
+    }
+
+
+`
